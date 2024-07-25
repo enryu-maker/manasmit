f7393e4bfafb4d43053d4f7560260bf449aae40c
tejastejale <tejastejale13@gmail.com>
2024-07-26 00:02:12 +0530

    ss

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -29,7 +29,7 @@ export default function Nav() {
         <div className="relative flex h-16 w-full items-center justify-end">
           <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
             {/* Mobile menu button */}
-            <DisclosureButton className="group sticky top-0 inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
+            <DisclosureButton className="group sticky top-0 inline-flex items-center justify-center rounded-md p-2  text-black focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
               <span className="absolute -inset-0.5" />
               <span className="sr-only">Open main menu</span>
               <Bars3Icon
